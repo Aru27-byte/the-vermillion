b1feddd9c9a1359790c513283096f6ec4c8361c3
Aru27-byte <27aroy@gmail.com>
2025-04-16 21:49:16 -0700

    few code changes to run the website

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/the-vermillion/', // Ensure the base path is set for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
